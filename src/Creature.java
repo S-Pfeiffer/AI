825a342672bdfdda987e7d5f51e10e18acf1e0df
S-Pfeiffer <courtroad.eighteen@hotmail.com>
2024-11-26 11:29:12 +0100

    1'st Pass

---
+++ src/Creature.java
@@ -0,0 +1,141 @@
+import java.util.List;
+
+public class Creature {
+
+    private Network network = new Network(Globals.INPUT_NEURONS, Globals.HIDDEN_NEURONS, Globals.OUTPUT_NEURONS);
+
+    // Inputs
+    private int age;
+    private double bias = 1.0;
+    private double foodValuePosition = 0.0;
+    private double foodValueFeeler = 0.0;
+    private double energy;
+    private int tileTypeFeeler;
+    private int tileTypePosition;
+
+    // Outputs
+    private double birth;
+    private double rotate;
+    private double move;
+    private double feelerAngle;
+    private double eat;
+
+    public void updateInputs() {
+        List<Neuron> inputNeurons = this.network.getInputs();
+        inputNeurons.get(0).setValue(this.bias);
+        inputNeurons.get(1).setValue(this.foodValuePosition);
+        inputNeurons.get(2).setValue(this.foodValueFeeler);
+        inputNeurons.get(3).setValue(this.energy);
+        inputNeurons.get(4).setValue(this.tileTypeFeeler);
+        inputNeurons.get(5).setValue(this.tileTypePosition);
+        inputNeurons.get(6).setValue(this.age);
+    }
+
+    public Creature() {
+        System.out.println(this.network.getInputs());
+    }
+
+    public Network getNetwork() {
+        return this.network;
+    }
+
+    public void setNetwork(Network network) {
+        this.network = network;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getBias() {
+        return bias;
+    }
+
+    public void setBias(double bias) {
+        this.bias = bias;
+    }
+
+    public double getFoodValuePosition() {
+        return foodValuePosition;
+    }
+
+    public void setFoodValuePosition(double foodValuePosition) {
+        this.foodValuePosition = foodValuePosition;
+    }
+
+    public double getFoodValueFeeler() {
+        return foodValueFeeler;
+    }
+
+    public void setFoodValueFeeler(double foodValueFeeler) {
+        this.foodValueFeeler = foodValueFeeler;
+    }
+
+    public double getEnergy() {
+        return energy;
+    }
+
+    public void setEnergy(double energy) {
+        this.energy = energy;
+    }
+
+    public int getTileTypeFeeler() {
+        return tileTypeFeeler;
+    }
+
+    public void setTileTypeFeeler(int tileTypeFeeler) {
+        this.tileTypeFeeler = tileTypeFeeler;
+    }
+
+    public int getTileTypePosition() {
+        return tileTypePosition;
+    }
+
+    public void setTileTypePosition(int tileTypePosition) {
+        this.tileTypePosition = tileTypePosition;
+    }
+
+    public double getBirth() {
+        return birth;
+    }
+
+    public void setBirth(double birth) {
+        this.birth = birth;
+    }
+
+    public double getRotate() {
+        return rotate;
+    }
+
+    public void setRotate(double rotate) {
+        this.rotate = rotate;
+    }
+
+    public double getMove() {
+        return move;
+    }
+
+    public void setMove(double move) {
+        this.move = move;
+    }
+
+    public double getFeelerAngle() {
+        return feelerAngle;
+    }
+
+    public void setFeelerAngle(double feelerAngle) {
+        this.feelerAngle = feelerAngle;
+    }
+
+    public double getEat() {
+        return eat;
+    }
+
+    public void setEat(double eat) {
+        this.eat = eat;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,7 @@
-/**
- * this project is intended to implement a simulation of AI creatures in Java
- *
- * @author Stephan Pfeiffer
- * @version 0.00
- */
-
-/**
- * The main Class of this project
- */
-
-
 public class Main {
     public static void main(String[] args) {
 
-        /**
-         * Call th constructor of "Network" to generate a
-         * new network called "network"
-         *
-         * @param inputNeurons, the count of neurons in the first layer
-         * @param hiddenNeurons, the count of neurons in the second layer
-         * @param OutputNeurons, the count of neurons in the third layer
-         * @return A network type Network
-         */
+       Creature creature = new Creature();
 
-        Network network = new Network(Globals.INPUT_NEURONS, Globals.HIDDEN_NEURONS, Globals.OUTPUT_NEURONS);
-
-        for (Neuron n : network.getInputs()) {
-            n.setValue(Tool.rndDouble(-1.0, 1.0));
-        }
-
-        for (Neuron n : network.getOutputs()) {
-            System.out.println(n.getValue());
-        }
-
-        network.feedForward();
-        System.out.println();
-
-        for (Neuron n : network.getOutputs()) {
-            System.out.println(n.getValue());
-        }
     }
 }
