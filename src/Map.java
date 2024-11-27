6742ca0ffba8349beea586be82c696cfc4791a90
S-Pfeiffer <courtroad.eighteen@hotmail.com>
2024-11-27 09:43:49 +0100

    V0.01

--- src/Map.java
+++ src/Map.java
@@ -1,10 +1,15 @@
 public class Map {
 
     private static int size;
-    private static int[][] tileMap;
+    private static Tile[][] tileMap;
 
     private void initMap(int size) {
-        this.tileMap = new int[size][size];
+        this.tileMap = new Tile[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                tileMap[i][j] = new Tile(0, Globals.MAX_FOOD_ON_TILE);
+            }
+        }
     }
 
     public Map() {
@@ -20,11 +25,11 @@ public class Map {
         Map.size = size;
     }
 
-    public static int[][] getTileMap() {
+    public static Tile[][] getTileMap() {
         return tileMap;
     }
 
-    public static void setTileMap(int[][] tileMap) {
+    public static void setTileMap(Tile[][] tileMap) {
         Map.tileMap = tileMap;
     }
 }
