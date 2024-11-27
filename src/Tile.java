fe3315da9f20b53ff8f93466d73b965653999ff7
S-Pfeiffer <courtroad.eighteen@hotmail.com>
2024-11-27 17:56:36 +0100

    V0.01

--- src/Tile.java
+++ src/Tile.java
@@ -1,2 +1,25 @@
 public class Tile {
+    private String type;
+    private double foodValue;
+
+    public Tile(String type, double foodValue) {
+        this.type = type;
+        this.foodValue = foodValue;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public double getFoodValue() {
+        return foodValue;
+    }
+
+    public void setFoodValue(double foodValue) {
+        this.foodValue = foodValue;
+    }
 }
