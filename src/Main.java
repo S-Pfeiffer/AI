5bb94b9adda611ea9ce0adc46bbb87e8ac08719f
S-Pfeiffer <courtroad.eighteen@hotmail.com>
2024-11-27 17:52:26 +0100

    V0.01

--- src/Main.java
+++ src/Main.java
@@ -36,65 +36,6 @@ public static void main(String[] args) {
 }
 
 public static void uiRenderer(GLRenderer renderer, long window) {
-    for (int x = 0; x < Globals.MAP_SIZE; x++) {
-        for (int y = 0; y < Globals.MAP_SIZE; y++) {
-            double xx = GLTools.glMapX(x * Globals.TILE_SIZE);
-            double yy = GLTools.glMapY(y * Globals.TILE_SIZE);
-            if (worldMap.getTile(x, y) == 0) {
-                renderer.fillRectangle(xx, yy, xx + Globals.TILE_SIZE, yy + Globals.TILE_SIZE, 0, 0, 1);
-            } else {
-                if (worldMap.getFood(x, y) < 99) {
-                    worldMap.setFood(x, y, worldMap.getFood(x, y) + 0.1d);
-                }
-                if (worldMap.getFood(x, y) < 1) {
-                    worldMap.setFood(x, y, 1);
-                }
-
-                renderer.fillRectangle(xx, yy, xx + Globals.TILE_SIZE, yy + Globals.TILE_SIZE, 0, worldMap.getFood(x, y) / 100, 0);
-            }
-        }
-    }
-    //Kreatur zeichnen
-    cx += Tool.rndDouble(-0.01f, 0.01f);
-    cy += Tool.rndDouble(-0.01f, 0.01f);
-
-    if (cx < -0.8) {
-        cx = -0.8;
-    }
-    if (cx > 0.8) {
-        cx = 0.8;
-    }
-    if (cy < -0.8) {
-        cy = -0.8;
-    }
-    if (cy > 0.8) {
-        cy = 0.8;
-    }
-
-
-    int tileX = (int) Tool.mapTo(cx, -1, 1, 0, Globals.MAP_SIZE);
-    int tileY = (int) Tool.mapTo(cy, -1, 1, 0, Globals.MAP_SIZE);
-    int tileT = worldMap.getTile(tileX, tileY);
-
-    if (tileT == 0) {
-        health--;
-        renderer.fillCircle(cx, cy, 0.02f, 4, 1, 1, 1);
-    } else {
-        if (worldMap.getFood(tileX, tileY) > 1) {
-            health++;
-        }
-        renderer.fillCircle(cx, cy, 0.02f, 20, 1, 1, 1);
-        worldMap.setFood(tileX, tileY, worldMap.getFood(tileX, tileY) - 1);
-    }
-
-    if (health <= 0) {
-        cx += Tool.rndDouble(-0.8, 0.8);
-        cy += Tool.rndDouble(-0.8, 0.8);
-        health = 100;
-    }
-    if (health >= 100) {
-        health = 100;
-    }
 
     glfwSwapBuffers(window);
 }
--- src/Map.java
+++ src/Map.java
@@ -1,8 +1,6 @@
 public class Map {
 
     private static int size;
-    private static int[][] tileMap;
-    private static double[][] foodMap;
 
     public Map() {
         size = Globals.MAP_SIZE;
@@ -10,54 +8,6 @@ public class Map {
     }
 
     private void initMap(int size) {
-        tileMap = new int[size][size];
-        foodMap = new double[size][size];
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                if (Tool.rndDouble(0f, 100f) > (float) Globals.MAP_LAND_RATIO) {
-                    tileMap[i][j] = 0;
-                    foodMap[i][j] = 0;
-                } else {
-                    tileMap[i][j] = 1;
-                    foodMap[i][j] = Globals.MAX_FOOD_ON_TILE;
-                }
-            }
-        }
-    }
 
-    public double getFood(int x, int y) {
-        return foodMap[x][y];
-    }
-
-    public void setFood(int x, int y, double food) {
-        foodMap[x][y] = (float) food;
-    }
-
-    public static int[][] getTileMap() {
-        return tileMap;
-    }
-
-    public static void setTileMap(int[][] tileMap) {
-        Map.tileMap = tileMap;
-    }
-
-    public void setTile(int x, int y, int type) {
-        tileMap[x][y] = type;
-    }
-
-    public int getTile(int x, int y) {
-        return tileMap[x][y];
-    }
-
-    public int getSize() {
-        return size;
-    }
-
-    public void setSize(int size) {
-        Map.size = size;
-    }
-
-    public void setTileMap(int x, int y, int tile) {
-        tileMap[x][y] = tile;
     }
 }
